29225863bc7715686206b631776a180c4d246acf
MustakimK <mustakimkazi10@gmail.com>
2024-05-13 15:42:08 -0400

    about me update

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -19,7 +19,7 @@ const About = () => {
                     My name is Mustakim Kazi and I&apos;m currently a 3rd year Computer Science student at McMaster University. 
                     I&apos;m passionate about software development and my portfolio reflects a diverse range of projects, including a 
                     custom UNIX shell, a full-stack Android application, a full-stack web application, and many more. I&apos;m actively 
-                    looking for a software engineering internship opportunity for the summer of 2024, and I would be more than happy to 
+                    looking for a software engineering internship opportunity for the fall of 2024, and I would be more than happy to 
                     answer any questions or explore potential collaborations. Please feel free to <a href="#contact" onClick={handleContactClick}>contact me</a> if you&apos;re interested!
                 </div>
                 <img src='./images/aboutme.jpg' alt="Picture of Me" className="about-pic" />
